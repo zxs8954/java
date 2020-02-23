2ab4c8798b078a2b2b827d77fbf0fc44c0243239
zxs8954 <15547929160@163.com>
2020-02-23 16:40:30 +0800

    asd

---
+++ List/src/main/java/com/thread26_27/reStudy/demo4/ThreadCallableDemo.java
@@ -0,0 +1,11 @@
+package com.thread26_27.reStudy.demo4;
+
+import java.util.concurrent.Callable;
+
+public class ThreadCallableDemo implements Callable<String> {
+    public String call(){
+        return "abc";
+    }
+
+
+}
